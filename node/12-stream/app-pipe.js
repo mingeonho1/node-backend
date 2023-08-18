2a48aed87b2f13bc20891b8dc1fa730b8e73e35a
HOYA <als77770@naver.com>
2023-08-18 21:20:59 +0900

    파일정리 스크립트

--- node/12-stream/app-pipe.js
+++ node/12-stream/app-pipe.js
@@ -1,17 +1,17 @@
-const fs = require('fs');
-const zlib = require('zlib');
+const fs = require("fs");
+const zlib = require("zlib");
 
-const readStream = fs.createReadStream('./file.txt');
+const readStream = fs.createReadStream("./file.txt");
 const zlibStream = zlib.createGzip();
-const writeStream = fs.createWriteStream('./file4.zip');
+const writeStream = fs.createWriteStream("./file4.zip");
 const piping = readStream.pipe(zlibStream).pipe(writeStream);
-piping.on('finish', () => {
-  console.log('done!!');
+piping.on("finish", () => {
+  console.log("done!!");
 });
 
-const http = require('http');
+const http = require("http");
 const server = http.createServer((req, res) => {
-  const stream = fs.createReadStream('./file.txt');
+  const stream = fs.createReadStream("./file.txt");
   stream.pipe(res);
 });
 server.listen(3000);
